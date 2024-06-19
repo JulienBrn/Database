2182cc878800d534fc29d3e1d0db0dc49728cf4a
JulienB <julienbraine@yahoo.fr>
2024-06-19 15:25:12 +0200

    first version

--- src/Pipeline/main.py
+++ src/Pipeline/main.py
@@ -2,7 +2,9 @@ import logging, beautifullogger
 import sys
 from Pipeline.pipeline import Pipeline
 import pandas as pd
+from pathlib import Path
 logger = logging.getLogger(__name__)
+
 
 def setup_nice_logging():
     beautifullogger.setup(logmode="w")
@@ -22,20 +24,42 @@ sys.excepthook = handle_exception
 
 
 p = Pipeline()
+base_folder= Path(".")
 
-@p.autocoord()
+@p.register_coord()
 def subject():
     return pd.DataFrame([["s1", "toto"], ["s2", "titi"]], columns=["subject", "stupid"])
 
-@p.autocoord()
-def session(subject):
+@p.register_coord()
+def session(subject, stupid):
     if subject == "s1":
-        return pd.DataFrame([["se1"], ["se2"]], columns=["session"])
+        return pd.DataFrame([["se1" + stupid], ["se2"]], columns=["session"])
     elif subject == "s2":
         return pd.DataFrame([["se3"]], columns=["session"])
+
+@p.register_data()
+class Song:
+
+    name = "song"
+    
+    @staticmethod
+    def location(session):
+        return list((base_folder / session / "song").glob("*.npy"))[0]
+    
+    @staticmethod
+    def compute(out_location, session):
+        import json
+        metadata = p.get_single_location("carmen_metadata", session=session)
+        if not metadata.exists():
+            p.compute("carmen_metadata", session=session)
+        metadata = json.load(metadata.open("r"))
+        return metadata["test"]*2
+
+
+
 
 def run():
     setup_nice_logging()
     logger.info("Running start")
-    print(p.get_coords("session"))
+    print(p.get_coords("session", subject=slice("s1", "s3")))
     logger.info("Running end")
--- src/Pipeline/pipeline.py
+++ src/Pipeline/pipeline.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 import pandas as pd
 from pathlib import Path
 import inspect
+import collections, collections.abc
+
 
 class CoordsProtocol(Protocol):
     def meth(self, coords: Dict[str, Any]) -> pd.DataFrame: ...
@@ -16,15 +18,13 @@ class DataLocProtocol(Protocol):
     def meth(self, coords: Dict[str, Any]) -> Path | List[Any]: ...
 
 class DataComputeProtocol(Protocol):
-    def meth(self, coords: Dict[str, Any], input_locations: Dict[str, pd.DataFrame]) -> Path | List[Any]: ...
+    def meth(self, out_location, coords: Dict[str, Any]) -> None: ...
 
 @dataclass
 class Data:
     dependencies: List[str]
-    location: Callable[[Dict[str, Any]], Path | List[Any]]
-
-    inputs: List[str]
-    computation: DataComputeProtocol
+    location: DataLocProtocol
+    computation: DataComputeProtocol | None
 
 class Pipeline:
     coords: Dict[str, Coords]
@@ -34,42 +34,104 @@ class Pipeline:
         self.coords={}
         self.data={}
 
-    def coord(self, name: str, depends_on: List[str] = None):
+
+    def declare_coord(self, name: str, depends_on: List[str], func):
+        self.coords[name] = Coords(dependencies=depends_on, func=func)
+
+
+    def declare_data(self, name, depends_on: List[str], loc_func: DataLocProtocol, compute_func: DataComputeProtocol=None):
+        self.data[name] = Data(dependencies=depends_on, location=loc_func, computation=compute_func)
+
+    def register_coord(self, name: str = None, depends_on: List[str] = None):
         def decorator(f):
-            self.coords[name] = Coords(dependencies=depends_on, func=f)
-            return f
-        return decorator
-    
-    def autocoord(self, name: str = None):
-        def decorator(f):
-            nonlocal name
-            depends_on = list(inspect.signature(f).parameters.keys())
+            nonlocal name, depends_on
+            if depends_on is None:
+                depends_on = list(inspect.signature(f).parameters.keys())
+                func = lambda coords: f(**{k: v for k,v in coords.items() if k in depends_on})
+            else:
+                func = f
             if name is None:
                 name = f.__name__
-            self.coords[name] = Coords(dependencies=depends_on, func=lambda coords: f(**coords))
+            self.declare_coord(name, depends_on=depends_on, func=func)
             return f
         return decorator
     
-    def get_coords(self, name):
-        coord = self.coords[name]
-        # print("entering")
-        df = pd.DataFrame([[]])
-        for d in coord.dependencies:
-            # print(df)
-            ddf = self.get_coords(d)
-            on = set(df.columns).intersection(set(ddf.columns))
-            df = df.merge(ddf, how="outer" if on else "cross", on=on if on else None)
-        # print(df)
-        # print("done d")
-        if df.empty:
-            return coord.func(coords={})
-        res = []
-        for _, row in df.iterrows():
-            tmp = coord.func(coords=row.loc[coord.dependencies].to_dict())
-            for c in row.index:
-                tmp[c] = row.at[c]
-            if not name in tmp.columns:
-                raise Exception(f'{name} should appear in columns...')
-            res.append(tmp)
-        return pd.concat(res)
+    def register_data(self, name=None, depends_on=None):
+        def decorator(cls):
+            nonlocal name, depends_on
+            if name is None:
+                if hasattr(cls, "name"):
+                    name = cls.name
+                else:
+                    name = cls.__name__
+            if depends_on is None:
+                depends_on = list(inspect.signature(cls.location).parameters.keys())[1:]
+                loc_func = lambda coords: cls.location(**{k: v for k,v in coords.items() if k in depends_on})
+                compute_func =  lambda out_location, coords: cls.compute(out_location, **{k: v for k,v in coords.items() if k in depends_on}) if hasattr(cls, "compute") else None
+            else:
+                loc_func = cls.location
+                compute_func = cls.compute if hasattr(cls, "compute") else None
+            self.declare_data(name, depends_on, loc_func=loc_func, compute_func=compute_func)
+            return cls
+        return decorator
 
+
+    def get_coords(self, names, selection_dict = {}, **selection_kwargs):
+        selection_dict = selection_dict | selection_kwargs
+        if isinstance(names, str):
+            names = (names, )
+        df = pd.DataFrame([[]])
+        names= set(d for n in names for d in self.coords[n].dependencies if d in self.coords).union(set(names))
+        while True:
+            names = names.difference(set(df.columns))
+            name = [n for n in names if set(self.coords[n].dependencies).issubset(set(df.columns))]
+            if len(name)==0:
+                break
+            else:
+                name=name[0]
+            coord = self.coords[name]
+
+            r = []
+            for _, row in df.iterrows():
+                tmp = coord.func(coords=row.to_dict())
+                for c in row.index:
+                    tmp[c] = row.at[c]
+                if not name in tmp.columns:
+                    raise Exception(f'{name} should appear in columns...')
+                r.append(tmp)
+            ddf= pd.concat(r)
+            print(ddf)
+            for s, v in selection_dict.items():
+                if s in ddf.columns:
+                    if isinstance(v, tuple):
+                        ddf = ddf.loc[ddf[s].isin(v)]
+                    elif isinstance(v, slice):
+                        if v.step:
+                            Exception("step not handled")
+                        start_cond = ddf[s] >= v.start if v.start else True
+                        end_cond = ddf[s] < v.stop if v.stop else True
+                        ddf = ddf.loc[start_cond & end_cond]
+                    else:
+                        ddf = ddf.loc[ddf[s] ==v]
+            on = set(df.columns).intersection(set(ddf.columns))
+            df = df.merge(ddf, how="outer" if on else "cross", on=list(on) if on else None)
+        return df
+    
+    def get_locations(self, name, selection_dict={}, **selection_kwargs) -> pd.Series:
+        coords = self.get_coords(self.data[name].dependencies, selection_dict, **selection_kwargs)
+        cols = coords.columns
+        coords["location"] = coords.apply(lambda row: self.data[name].location(row.to_dict()), axis=1)
+        return coords.set_index(cols)
+            
+
+    def get_single_location(self, name, selection_dict={}, **selection_kwargs) -> Path:
+        r = self.get_locations(name, selection_dict, **selection_kwargs)
+        if len(r.index) !=1:
+            raise Exception("Problem")
+        else:
+            return r.iat[0]
+    
+    def compute(self, name,  selection_dict={}, **selection_kwargs) -> None:
+        locs = self.get_locations(name, selection_dict, **selection_kwargs).reset_index()
+        for _, row in locs.items():
+            self.data[name].computation(row.iat[-1], row.iloc[:-1].to_dict())
